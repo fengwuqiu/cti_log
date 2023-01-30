9dae963e0068b6939dea026e1af8368ee84131e1
吴勇峰 <wuyongfeng@ctirobot.com>
2023-01-30 17:59:35 +0800

    humble

--- ctilog/.make_deb.sh
+++ ctilog/.make_deb.sh
@@ -2,10 +2,10 @@
 set -e
 CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 rm -fr debian obj-x86_64-linux-gnu
-bloom-generate rosdebian --os-name ubuntu --ros-distro foxy
+bloom-generate rosdebian --os-name ubuntu --ros-distro humble
 sed -i 's/$@/$@ --parallel/g' debian/rules
 PKG_NAME=$(cat package.xml| grep "<name>" | awk -F '>' '{print $2}' | awk -F '<'  '{print $1}')
-PKG_NAME=$(echo "ros-foxy-${PKG_NAME}" | tr '_' '-')
+PKG_NAME=$(echo "ros-humble-${PKG_NAME}" | tr '_' '-')
 VERSION=$(cat package.xml| grep "<version>" | awk -F '>' '{print $2}' | awk -F '<'  '{print $1}')
 export TZ="Asia/Shanghai"
 STAMP=$(date  +'%Y%m%d.%H%M%S')
